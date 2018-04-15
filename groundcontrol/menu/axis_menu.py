f1671c955e81ff6ce7c29ccd6f1c5fb135b130ec
Basil Huber <basil.huber@gmail.com>
2018-04-15 18:08:19 +0200

    axis_menu: updated

--- groundcontrol/menu/axis_menu.py
+++ groundcontrol/menu/axis_menu.py
@@ -3,28 +3,31 @@ import tkinter as tk
 
 class AxisMenu(tk.Frame):
     DEFAULT_VALUE_WIDTH = 6
-    def __init__(self, parent):
+    FONT_SIZE_NAME = 12
+    def __init__(self, parent, name):
         super().__init__(parent)
 
+        tk.Label(self, text=name, font=(None, self.FONT_SIZE_NAME)).pack()
         tk.Label(self, text='Y axis').pack(side=tk.TOP)
-
-        min_frame = tk.Frame(self)
-        self.min_checked = tk.IntVar()
-        self.min_val = tk.DoubleVar()
-        self.min_cb = tk.Checkbutton(min_frame, text='min', variable=self.min_checked)
-        self.min_cb.pack(side=tk.LEFT)
-        self.min_entry = tk.Entry(min_frame, textvariable=self.min_val, width=self.DEFAULT_VALUE_WIDTH)
-        self.min_entry.pack(side=tk.RIGHT)
-        min_frame.pack()
 
         max_frame = tk.Frame(self)
         self.max_checked = tk.IntVar()
         self.max_val = tk.DoubleVar()
-        self.max_cb = tk.Checkbutton(max_frame, text='max', variable=self.max_checked)
-        self.max_cb.pack(side=tk.LEFT)
-        self.max_entry = tk.Entry(max_frame, textvariable=self.max_val, width=self.DEFAULT_VALUE_WIDTH)
-        self.max_entry.pack(side=tk.RIGHT)
+        max_cb = tk.Checkbutton(max_frame, text='max', variable=self.max_checked)
+        max_cb.pack(side=tk.LEFT)
+        max_entry = tk.Entry(max_frame, textvariable=self.max_val, width=self.DEFAULT_VALUE_WIDTH)
+        max_entry.pack(side=tk.RIGHT)
         max_frame.pack()
+
+        min_frame = tk.Frame(self)
+        self.min_checked = tk.IntVar()
+        self.min_val = tk.DoubleVar()
+        min_cb = tk.Checkbutton(min_frame, text='min', variable=self.min_checked)
+        min_cb.pack(side=tk.LEFT)
+        min_entry = tk.Entry(min_frame, textvariable=self.min_val, width=self.DEFAULT_VALUE_WIDTH)
+        min_entry.pack(side=tk.RIGHT)
+        min_frame.pack()
+
 
     def get_limits(self):
         min_lim = None
