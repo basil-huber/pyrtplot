fc2a34be36fdc5040e5d224ad7a7b9b765cfa979
Basil Huber <basil.huber@gmail.com>
2018-04-15 18:18:03 +0200

    rt_plot

--- groundcontrol/plotting/rt_plot.py
+++ groundcontrol/plotting/rt_plot.py
@@ -1,6 +1,7 @@
 from groundcontrol.utils import CircBuffer
 from pylab import *
 import tkinter as tk
+from _tkinter import TclError
 import matplotlib
 matplotlib.use("TkAgg")
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
@@ -10,22 +11,30 @@ import time
 from groundcontrol.menu import AxisMenu
 
 class RtPlot(tk.Frame, threading.Thread):
+    REFRESH_PERIOD = 0.01 # [s]
+
     def __init__(self, parent, data_buffer_dict, x_buffer, lock):
         tk.Frame.__init__(self, parent)
         threading.Thread.__init__(self)
 
         self.x_buffer = x_buffer
         self.lock = lock
-        self.axis_menu = AxisMenu(self)
-        self.axis_menu.pack(side=tk.LEFT)
+
+        self.axis_menu_frame = tk.Frame(self)
 
         self.subplots_dict = {}
         self.fig, self.axes = subplots(len(data_buffer_dict),1, sharex=True)
+        tk.Label(self.axis_menu_frame).pack(fill=tk.BOTH, expand=True)
         for i,var in enumerate(sorted(data_buffer_dict)):
             sp = {}
             sp['axis'] = self.axes[i]
             sp['buffer'] = data_buffer_dict[var]
+            sp['axis_menu'] = AxisMenu(self.axis_menu_frame, var)
+            sp['axis_menu'].pack()#fill=tk.Y, expand=True)
+            tk.Label(self.axis_menu_frame).pack(fill=tk.BOTH, expand=True)    
             self.subplots_dict[var] = sp
+        self.axis_menu_frame.pack(side=tk.LEFT, fill=tk.Y)
+        self.fig.tight_layout()
 
         # self.draw_fig()
 
@@ -34,16 +43,17 @@ class RtPlot(tk.Frame, threading.Thread):
         self.canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
 
     def run(self):
-        while True:
+        self.running = True
+        while self.running:
             try:
                 self.update()
-            except:
-                break
+            except TclError:
+               break
 
     def update(self):
         self.draw_fig()
         self.canvas.draw()
-        time.sleep(0.01)
+        time.sleep(self.REFRESH_PERIOD)
 
     def draw_fig(self):
         for sp in self.subplots_dict.values():
@@ -54,4 +64,4 @@ class RtPlot(tk.Frame, threading.Thread):
             axis.plot(self.x_buffer.head_view(500),sp['buffer'].head_view(500))
             if self.lock:
                 self.lock.release()
-            axis.set_ylim(self.axis_menu.get_limits())
+            axis.set_ylim(sp['axis_menu'].get_limits())
