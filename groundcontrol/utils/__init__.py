1335eb28324e2991edaba5e1967eba1795657fa9
Basil Huber <basil.huber@gmail.com>
2018-04-15 14:35:13 +0200

    Added circbuffer

---
+++ groundcontrol/utils/__init__.py
@@ -0,0 +1,1 @@
+from .circbuffer import CircBuffer
---
+++ groundcontrol/utils/circbuffer.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+import numpy as np
+
+class CircBuffer():
+    def __init__(self, max_len, shape=(0,0), dtype=float):
+        self.max_len = max_len
+        self.head = -1
+        self.length = 0
+        self.array = np.ndarray(shape=shape, dtype=float)
+        self.array.fill(np.nan)
+
+    def push(self, value):
+        if self.length < self.max_len:
+            self.head += 1
+            self.array[self.head] = value
+            self.length += 1
+        else:
+            self.head = (self.head + 1) % (self.length)
+            self.array[self.head] = value
+
+    def head_view(self,count):
+        tail = self.head + 1 - count
+        if tail >= 0:
+            return self.array[tail:self.head+1]
+        elif self.length < self.max_len:
+            return self.array[0:self.head+1]
+        else:
+            return np.concatenate((self.array[tail:self.length+1],self.array[0:self.head+1]))#, self.array[tail:self.length+1])
+
+
