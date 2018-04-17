84aee9754a3d17a352c1988676dbee5a29d899f2
Basil Huber <basil.huber@gmail.com>
2018-04-17 19:28:46 +0200

    menu: introduced window and mainwindow

--- groundcontrol/menu/main.py
+++ groundcontrol/menu/main.py
@@ -1,45 +1,117 @@
 #!/usr/bin/env python3
-
+import tkinter as tk
+import threading
+from groundcontrol.plotting.rt_plot import RtPlot
 from flightlog.parser import LineParser
+from groundcontrol.utils import CircBuffer, BufferIndexedCollection
+import time
+
+
+
+class SubWindow(tk.Toplevel):
+    def __init__(self, parent, checkbox_var):
+        tk.Toplevel.__init__(self, parent)
+        self.is_visible = True
+        self.checkbox_var = checkbox_var
+        self.protocol("WM_DELETE_WINDOW", self.on_closing)
+
+    def toggle_visible(self):
+        if self.is_visible:
+            self.withdraw()
+        else:
+            self.deiconify()
+        self.is_visible = not self.is_visible
+    def on_closing(self):
+        if self.is_visible:
+            self.toggle_visible()
+            self.checkbox_var.set(0)
+
+from tkinter import messagebox
+class MainWindow(tk.Tk):
+    def __init__(self, buffers):
+        tk.Tk.__init__(self)
+        self.buffers = buffers
+
+        tk.Label(self, text="Houston").pack()
+
+        self.checkbox_values = {}
+        for group_name in sorted(buffers.keys()):
+            self.checkbox_values[group_name] = tk.IntVar()
+            b = tk.Checkbutton(self, text=group_name,
+                command=lambda group_name=group_name: self.com(group_name),
+                variable=self.checkbox_values[group_name])
+            b.pack(side="top")
+
+        self.plot_windows = {}
+
+        self.protocol("WM_DELETE_WINDOW", self.on_closing)
+    def on_closing(self):
+        if messagebox.askokcancel("Quit", "Do you want to quit?"):
+            for rt_plot in self.plot_windows.values():
+                rt_plot.quit()
+            self.destroy()
+
+
+    def com(self, group_name):
+        if not group_name in self.plot_windows:
+            window = SubWindow(self, self.checkbox_values[group_name])
+            window.title(group_name)
+            rt_plot = RtPlot(window, buffers[group_name])
+            rt_plot.pack(fill=tk.BOTH, expand=True)
+            rt_plot.start()
+            self.plot_windows[group_name] = window
+        else:
+            self.plot_windows[group_name].toggle_visible()
 
 
 filename = 'LOG_0.LOG'
 
+max_len = 1000
+
 with LineParser(filename) as parser:
     groups = parser.parse_groups()
-    # change keys from indices to names
-    groups = {g['name'] : g for g in groups.values()}
+    buffers = {}
+    for group in groups.values():
+        variables = group['variables']
+        #var_list = [(var, variables[var]['nb']) for var in variables]
+        i = 0
+        var_list = []
+        for var in variables:
+            var_list.append((var, variables[var]['nb']))
+            i+= 1
+            if i >= 3:
+                break
+        buffers[group['name']] = BufferIndexedCollection(var_list, max_len)
+
+    group_buffer_indices = {group: 1 for group in buffers}
+    class ParserThread(threading.Thread):
+        def __init__(self, daemon=True):
+            threading.Thread.__init__(self, daemon=True)
+        def run(self):
+            try:
+                for entry in parser:
+                    var_name = entry[0]
+                    values = []
+                    i0 = 0
+                    for var in buffers[var_name].variables:
+                        i1 = i0 + var[1]
+                        values.append(entry[1][i0:i1])
+                        i0 = i1
+                    buffers[var_name].push(group_buffer_indices[var_name], values)
+                    group_buffer_indices[var_name] += 1
+
+                    time.sleep(0.0001)
+            except ValueError:
+                pass
 
 
-from tkinter import *
-from tkinter import ttk
-from groundcontrol.plotting.rt_plot import RtPlot
 
-plot_windows = {}
-def com(group_name):
-    if not group_name in plot_windows:
-        window = Toplevel(root)
-        window.title(group_name)
-        rt_plot = RtPlot(window)
+    
+    
 
+    parser_thread = ParserThread()
+    parser_thread.start()
 
-        plot_windows[group_name] = window
-    else:
-        plot_windows[group_name].destroy()
-        del plot_windows[group_name]
-
-root = Tk()
-
-w = Label(root, text="Houston")
-w.pack()
-
-
-for g in sorted(groups.keys()):
-    b = Checkbutton(root, text=g, command = lambda group_name=g: com(group_name))
-    b.pack(side="top")
-
-
-
-
-
-root.mainloop()
+    root = MainWindow(buffers)
+    root.mainloop()
+    
